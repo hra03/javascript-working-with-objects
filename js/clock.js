6293b5d84fd3e7f9720fa146679b8829faee42eb
Heera Rana <heera.rana2@skybettingandgaming.com>
2022-09-20 16:22:44 +0100

    clockgame

---
+++ js/clock.js
@@ -0,0 +1,14 @@
+(function(){
+console.info("Hello JS")
+var myNode = document.createElement("div");
+document.getElementById("digitalClock").appendChild(myNode);
+
+var updateTime = function(){ 
+    console.info('tick tock'); 
+    var myDate = new Date();
+myNode.innerHTML = myDate.toTimeString().substring(0, 8);
+   } 
+   setInterval(updateTime, 1000);
+updateTime();
+
+})();
